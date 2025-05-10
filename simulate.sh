14c2d5a5c40bba1991288cacd99dda95651b0b40
Sammy L <no@example.com>
2025-05-10 14:52:16 -0500

    Move "add_bin_to_path.sh" to "simulate.sh" to better show what it does.

--- simulate.sh
+++ simulate.sh
@@ -2,4 +2,5 @@
 # Usage: source ./add_bin_to_path.sh
 # Temporarily adds ./bin to the beginning of your $PATH for this shell session.
 export PATH="$(pwd)/bin:$PATH"
-echo "Added $(pwd)/bin to PATH for this session." 
+echo "Added $(pwd)/bin to PATH for this session. Entering highway shell..." 
+highway
