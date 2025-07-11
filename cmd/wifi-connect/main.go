d96ebd2c7685db5076d320beffd228c04cb9b57b
Sam L <no@nope.nicetry>
2025-07-11 03:45:06 -0500

    she is hiding in your wifi

--- cmd/wifi-connect/main.go
+++ cmd/wifi-connect/main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/mdlayher/wifi"
-	"pifke.org/wpasupplicant"
 )
 
 // wifi-connect: connects to a WiFi network using WPA2-PSK
@@ -60,41 +59,24 @@ func main() {
 		fmt.Fprintf(os.Stderr, "wifi-connect: SSID '%s' not found\n", ssid)
 		os.Exit(1)
 	}
-	// Use wpasupplicant to connect
-	wpa, err := wpasupplicant.Unixgram(ifi.Name)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "wifi-connect: cannot connect to wpa_supplicant:", err)
-		os.Exit(1)
-	}
-	defer wpa.Close()
-	// Add network and set credentials
-	netID, err := wpa.AddNetwork()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "wifi-connect: failed to add network:", err)
-		os.Exit(1)
-	}
-	if err := wpa.SetNetwork(netID, "ssid", ssid); err != nil {
-		fmt.Fprintln(os.Stderr, "wifi-connect: failed to set SSID:", err)
-		os.Exit(1)
-	}
-	if err := wpa.SetNetwork(netID, "psk", password); err != nil {
-		fmt.Fprintln(os.Stderr, "wifi-connect: failed to set password:", err)
-		os.Exit(1)
-	}
-	if err := wpa.SelectNetwork(netID); err != nil {
-		fmt.Fprintln(os.Stderr, "wifi-connect: failed to select network:", err)
-		os.Exit(1)
-	}
+
+	// Use github.com/mdlayher/wifi to connect
 	fmt.Printf("Connecting to SSID '%s' on interface '%s'...\n", ssid, ifi.Name)
-	// Wait for connection (poll status)
-	for i := 0; i < 20; i++ {
-		status, err := wpa.Status()
-		if err == nil && status.WPAState() == "COMPLETED" {
-			fmt.Println("Successfully connected to WiFi network.")
-			return
-		}
-		time.Sleep(500 * time.Millisecond)
+	if err := client.ConnectWPAPSK(ifi, ssid, password); err != nil {
+		fmt.Fprintln(os.Stderr, "wifi-connect: failed to connect:", err)
+		os.Exit(1)
 	}
-	fmt.Fprintln(os.Stderr, "wifi-connect: connection timed out or failed.")
-	os.Exit(1)
+
+	bss, err := client.BSS(ifi)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "wifi-connect: failed to get BSS information after connecting:", err)
+		os.Exit(1)
+	}
+
+	if bss.Status == wifi.BSSStatusAssociated && bss.SSID == ssid {
+		fmt.Println("Successfully connected to WiFi network.")
+	} else {
+		fmt.Fprintf(os.Stderr, "wifi-connect: connection to '%s' failed, status: %s\n", ssid, bss.Status)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	golang.org/x/net v0.40.0
 	golang.org/x/sys v0.33.0
 	golang.org/x/term v0.32.0
-	pifke.org/wpasupplicant v0.0.0-20221018205742-4b5b2dde8b55
 )
 
 require (
